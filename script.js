cfc2261b6b0fcaac9dfe0264e6d595846bdee3c5
KhaledElbasiouni <khaled.elbasiouni222@gmail.com>
2022-06-17 15:09:06 +0300

    Create basic UI

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict'
 
+let playerScore = 0;
+let compScore = 0;
 function computerPlay() {
     let states = ["rock", "paper", "scissors"];
     let randomState = Math.floor(Math.random()*3);
@@ -16,8 +18,10 @@ function playRound(playerSelection, computerSelection) {
     }else if(winner === "Invalid State"){
         return "Invalid Input! Please choose from Rock, Paper, Or Scissors :)";
     }else if (winner === playerSelection){
+        playerScore++;
         return `You Win! ${playerSelection} beats ${computerSelection}`;
     }else {
+        compScore++;
         return `You Lose! ${computerSelection} beats ${playerSelection}`;
     }
 }
@@ -48,11 +52,39 @@ function compareState(firstState, secondState) {
     return "Invalid State"
 }
 
-
-function game() {
-    for(let i = 0; i < 5; i++){
-        console.log(playRound(prompt("Pick your weapon!", ""), computerPlay()));
-    }
+function resetGame(){
+    buttons.forEach(btn => btn.disabled = false);
+    playerScore = 0;
+    compScore = 0;
+    pScore.textContent = '';
+    cScore.textContent = '';
+    result.textContent = '';
+    winner.textContent = '';
 }
 
-game();
+
+
+const pScore = document.querySelector('.player-score span');
+const cScore = document.querySelector('.comp-score span');
+const result = document.querySelector('.result');
+const winner = document.querySelector('.winner');
+const reset = document.querySelector('.reset');
+
+const buttons = document.querySelectorAll('.selection');
+buttons.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        result.textContent = playRound(e.target.textContent,computerPlay());
+        pScore.textContent = playerScore;
+        cScore.textContent = compScore;
+        if(playerScore === 5){
+            winner.textContent = 'PLAYER WINS! BOT LOSES! (OBVIOUSLY)';
+            buttons.forEach(btn=>btn.disabled = true);
+        }else if(compScore === 5){
+            buttons.forEach(btn=>btn.disabled = true);
+            winner.textContent = 'BOT WINS! HUMAN LOSES! (01011101101)';
+        }
+        // console.log(e.target.textContent);
+    })
+});
+
+reset.addEventListener('click', resetGame);
